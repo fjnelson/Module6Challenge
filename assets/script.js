8b37e31ed21515f31936e6e963ec056213d75d65
fjnelson <97076779+fjnelson@users.noreply.github.com>
2022-11-22 22:31:10 -0800

    loading scripts and links

---
+++ assets/script.js
@@ -0,0 +1,31 @@
+//API Call
+//api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+
+//example of API Call
+//api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid={API key}
+
+// //user story
+// AS A traveler
+// I WANT to see the weather outlook for multiple cities
+// SO THAT I can plan a trip accordingly
+
+//acceptance criteria
+// GIVEN a weather dashboard with form inputs
+
+// WHEN I search for a city
+// THEN I am presented with current and future conditions for that city and that city is added to the search history
+
+// WHEN I view current weather conditions for that city
+// THEN I am presented with the city name, the date, an icon representation of weather conditions, 
+// the temperature, the humidity, and the the wind speed
+
+// WHEN I view future weather conditions for that city
+// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, 
+// the temperature, the wind speed, and the humidity
+
+// WHEN I click on a city in the search history
+// THEN I am again presented with current and future conditions for that city
+
+//technical acceptance
+// Uses the OpenWeather API to retrieve weather data
+// Uses localStorage to store persistent data
